cfde44293412f4500621d02bfdeb52984c159c73
Iqbol Toshmatov <toshmatovdeveloper1105@gmail.com>
2026-09-05 16:35:46 +0500

    Added base entities.

---
+++ GeoDelivery/Catalog/Domain/Dish.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.Domain;
+
+public class Dish(Guid restaurantId, Guid categoryId, string name, decimal price)  
+{  
+    private Dish() : this(Guid.Empty, Guid.Empty, string.Empty, 0m) { }  
+      
+    public Guid Id { get; set; } = Guid.CreateVersion7();  
+      
+    public Guid RestaurantId { get; set; } = restaurantId;  
+  
+    public Guid CategoryId { get; set; } = categoryId;  
+  
+    public string Name { get; set; } = name;  
+  
+    public decimal Price { get; set; } = price;  
+  
+    public bool IsAvailable { get; set; } = true;  
+}
---
+++ GeoDelivery/Catalog/Domain/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.Domain;
+
+public class Restaurant(string name, string description)  
+{  
+    private Restaurant() : this(string.Empty, string.Empty) { }  
+      
+    public Guid Id { get; init; } = Guid.CreateVersion7();  
+      
+    public string Name { get; init; } = name;  
+  
+    public string Description { get; init; } = description;  
+  
+    public bool IsActive { get; init; } = true;  
+  
+    private readonly List<Category> _categories = new();  
+}
