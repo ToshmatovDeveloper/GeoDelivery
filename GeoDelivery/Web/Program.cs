11ecebf0c2c146e95346284f43dd9ff2c8954f7d
Iqbol Toshmatov <toshmatovdeveloper1105@gmail.com>
2026-09-09 21:50:53 +0500

    add structured logging with Serilog and sink telemetry to Seq

--- GeoDelivery/Web/Program.cs
+++ GeoDelivery/Web/Program.cs
@@ -1,8 +1,14 @@
 using Catalog.Application.Features.Restaurant.Command;
 using Catalog.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Serilog.Debugging.SelfLog.Enable(Console.Error);
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 var catalogDbConnectionString = builder.Configuration.GetConnectionString("CatalogDbConnectionString");
 
@@ -10,11 +16,8 @@ builder.Services.AddDbContext<CatalogDbContext>(options =>
     options.UseNpgsql(catalogDbConnectionString)
         .UseSnakeCaseNamingConvention()); 
 
-builder.Services.AddMediatR
-    (
-        cfg => cfg
-            .RegisterServicesFromAssemblyContaining<CreateRestaurantCommand>()
-    );
+builder.Services.AddMediatR (cfg => cfg
+        .RegisterServicesFromAssemblyContaining<CreateRestaurantCommand>());
 
 builder.Services.AddControllers();
 
