3e7388ec0b23e5bf4afd20e33644b2e406b648d8
Iqbol Toshmatov <toshmatovdeveloper1105@gmail.com>
2026-09-22 13:52:18 +0500

    Added base entities for auth module.

---
+++ GeoDelivery/Auth/Domain/RefreshToken.cs
@@ -0,0 +1,16 @@
+﻿namespace Auth.Domain;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    
+    public string Token  { get; set; }
+    
+    public Guid UserId { get; set; }
+    
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    
+    public DateTime ExpiresOnUtc { get; set; }
+    
+    public User User { get; set; }
+}
